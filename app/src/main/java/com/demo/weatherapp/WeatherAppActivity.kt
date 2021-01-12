309270b569aa94d15537953caf2398abe9daaebd
Paul Allan <pallan@bamboorose.com>
2021-01-12 19:52:47 +0000

    * Basic UI

--- app/src/main/java/com/demo/weatherapp/WeatherAppActivity.kt
+++ app/src/main/java/com/demo/weatherapp/WeatherAppActivity.kt
@@ -9,6 +9,7 @@ class WeatherAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.weather_app_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                     .replace(R.id.container, WeatherFragment.newInstance())
